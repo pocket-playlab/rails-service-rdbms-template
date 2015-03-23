ffc608d6859faf5cdf4d72f6ba40ca94cfe8e81e
Omar Khan <omar@omarkhan.me>
2015-03-23 11:49:33 +0700

    Add SENTRY_SERVICE variable

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,7 @@
 Raven.configure do |config|
   config.environments = ['production']
-  config.tags = { environment: ENV.fetch('SENTRY_ENVIRONMENT', 'unknown') }
+  config.tags = {
+    service:     ENV.fetch('SENTRY_SERVICE',     'unknown'),
+    environment: ENV.fetch('SENTRY_ENVIRONMENT', 'unknown'),
+  }
 end
